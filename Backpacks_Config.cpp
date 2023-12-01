3531d4825a4e121cecc62f421ed7ea63c440916e
Veta24 <82298916+Veta24@users.noreply.github.com>
2023-11-30 20:08:37 -0600

    Update Backpacks_Config.cpp

--- Backpacks_Config.cpp
+++ Backpacks_Config.cpp
@@ -31,11 +31,11 @@ class CfgVehicles
 //Woodland Rucksacks
 	class 24th_Druid_Rucksack_Basic_Woodland: OPTRE_UNSC_Rucksack
 	{
-		dlc="24th Aux";
-		author="Ithias";
+		dlc="24th RB Aux";
+		author="Veta";
         scope=2;
         scopeCurator=2;
-		displayName="[24th] Athena Rucksack (Basic / Woodland)";
+		displayName="[24th RB] Druid Rucksack (Basic / Woodland)";
 		model="\OPTRE_unsc_units\army\rucksack.p3d";
 		maximumLoad=350;
 		mass=40;
@@ -49,16 +49,16 @@ class CfgVehicles
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"24th_AB_AUX\Equipment\Backpacks\Tex\24th_Athena_Rucksack_Woodland.paa",
+			"24th_RB_AUX\Equipment\Backpacks\Tex\24th_Druid_Rucksack_Woodland.paa",
 			""
 		};
 	};
 
 	class 24th_Druid_Rucksack_Heavy_Woodland: 24th_Druid_Rucksack_Basic_Woodland
 	{
-		dlc="24th Aux";
-		author="Ithias";
-		displayName="[24th] Athena Rucksack (Heavy / Woodland)";
+		dlc="24th RB Aux";
+		author="Veta";
+		displayName="[24th RB] Druid Rucksack (Heavy / Woodland)";
 		maximumLoad=400;
 		mass=50;
 		hiddenSelections[]=
@@ -72,9 +72,9 @@ class CfgVehicles
 
 	class 24th_Druid_Rucksack_Medic_Woodland: 24th_Druid_Rucksack_Basic_Woodland
 	{
-		dlc="24th Aux";
-		author="Ithias";
-		displayName="[24th] Athena Rucksack (Medic / Woodland)";
+		dlc="24th RB Aux";
+		author="Veta";
+		displayName="[24th RB] Druid Rucksack (Medic / Woodland)";
         maximumLoad=400;
         mass=50;
 		hiddenSelections[]=
@@ -88,11 +88,11 @@ class CfgVehicles
 
 	class 24th_Druid_Rucksack_Basic_Woodland_LR: 24th_Druid_Rucksack_Basic_Woodland
 	{
-		dlc="24th Aux";
-		author="Ithias";
+		dlc="24th RB Aux";
+		author="Veta";
         scope=2;
         scopeCurator=2;
-		displayName="[24th] Athena Rucksack (NCO LR / Woodland)";
+		displayName="[24th RB] Druid Rucksack (NCO LR / Woodland)";
 		model="\OPTRE_unsc_units\army\rucksack.p3d";
 		maximumLoad=350;
 		mass=40;
@@ -106,7 +106,7 @@ class CfgVehicles
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"24th_AB_AUX\Equipment\Backpacks\Tex\24th_Athena_Rucksack_Woodland_LR.paa",
+			"24th_RB_AUX\Equipment\Backpacks\Tex\24th_Druid_Rucksack_Woodland_LR.paa",
 			""
 		};
 		tf_isolatedAmount=0.64999998;
@@ -121,11 +121,11 @@ class CfgVehicles
 //Urban Rucksacks
 	class 24th_Druid_Rucksack_Basic_Urban: OPTRE_UNSC_Rucksack
 	{
-		dlc="24th Aux";
-		author="Ithias";
+		dlc="24th RB Aux";
+		author="Veta";
         scope=2;
         scopeCurator=2;
-		displayName="[24th] Athena Rucksack (Basic / Urban)";
+		displayName="[24th RB] Druid Rucksack (Basic / Urban)";
 		model="\OPTRE_unsc_units\army\rucksack.p3d";
 		maximumLoad=350;
 		mass=40;
@@ -139,16 +139,16 @@ class CfgVehicles
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"24th_AB_AUX\Equipment\Backpacks\Tex\24th_Athena_Rucksack_Urban.paa",
+			"24th_RB_AUX\Equipment\Backpacks\Tex\24th_Druid_Rucksack_Urban.paa",
 			""
 		};
 	};
 
 	class 24th_Druid_Rucksack_Heavy_Urban: 24th_Druid_Rucksack_Basic_Urban
 	{
-		dlc="24th Aux";
-		author="Ithias";
-		displayName="[24th] Athena Rucksack (Heavy / Urban)";
+		dlc="24th RB Aux";
+		author="Veta";
+		displayName="[24th RB] Druid Rucksack (Heavy / Urban)";
 		maximumLoad=400;
 		mass=50;
 		hiddenSelections[]=
@@ -162,9 +162,9 @@ class CfgVehicles
 
 	class 24th_Druid_Rucksack_Medic_Urban: 24th_Druid_Rucksack_Basic_Urban
 	{
-		dlc="24th Aux";
-		author="Ithias";
-		displayName="[24th] Athena Rucksack (Medic / Urban)";
+		dlc="24th RB Aux";
+		author="Veta";
+		displayName="[24th RB] Druid Rucksack (Medic / Urban)";
         maximumLoad=400;
         mass=50;
 		hiddenSelections[]=
@@ -178,11 +178,11 @@ class CfgVehicles
 
 	class 24th_Druid_Rucksack_Basic_Urban_LR: 24th_Druid_Rucksack_Basic_Urban
 	{
-		dlc="24th Aux";
-		author="Ithias";
+		dlc="24th RB Aux";
+		author="Veta";
         scope=2;
         scopeCurator=2;
-		displayName="[24th] Athena Rucksack (NCO LR / Urban)";
+		displayName="[24th RB] Druid Rucksack (NCO LR / Urban)";
 		model="\OPTRE_unsc_units\army\rucksack.p3d";
 		maximumLoad=350;
 		mass=40;
@@ -196,7 +196,7 @@ class CfgVehicles
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"24th_AB_AUX\Equipment\Backpacks\Tex\24th_Athena_Rucksack_Urban_LR.paa",
+			"24th_RB_AUX\Equipment\Backpacks\Tex\24th_Druid_Rucksack_Urban_LR.paa",
 			""
 		};
 		tf_isolatedAmount=0.64999998;
@@ -211,11 +211,11 @@ class CfgVehicles
 //Desert Rucksacks
 	class 24th_Druid_Rucksack_Basic_Desert: OPTRE_UNSC_Rucksack
 	{
-		dlc="24th Aux";
-		author="Ithias";
+		dlc="24th RB Aux";
+		author="Veta";
         scope=2;
         scopeCurator=2;
-		displayName="[24th] Athena Rucksack (Basic / Desert)";
+		displayName="[24th RB] Druid Rucksack (Basic / Desert)";
 		model="\OPTRE_unsc_units\army\rucksack.p3d";
 		maximumLoad=350;
 		mass=40;
@@ -229,16 +229,16 @@ class CfgVehicles
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"24th_AB_AUX\Equipment\Backpacks\Tex\24th_Athena_Rucksack_Desert.paa",
+			"24th_RB_AUX\Equipment\Backpacks\Tex\24th_Druid_Rucksack_Desert.paa",
 			""
 		};
 	};
 
 	class 24th_Druid_Rucksack_Heavy_Desert: 24th_Druid_Rucksack_Basic_Desert
 	{
-		dlc="24th Aux";
-		author="Ithias";
-		displayName="[24th] Athena Rucksack (Heavy / Desert)";
+		dlc="24th RB Aux";
+		author="Veta";
+		displayName="[24th RB] Druid Rucksack (Heavy / Desert)";
 		maximumLoad=400;
 		mass=50;
 		hiddenSelections[]=
@@ -252,9 +252,9 @@ class CfgVehicles
 
 	class 24th_Druid_Rucksack_Medic_Desert: 24th_Druid_Rucksack_Basic_Desert
 	{
-		dlc="24th Aux";
-		author="Ithias";
-		displayName="[24th] Athena Rucksack (Medic / Desert)";
+		dlc="24th RB Aux";
+		author="Veta";
+		displayName="[24th RB] Druid Rucksack (Medic / Desert)";
         maximumLoad=400;
         mass=50;
 		hiddenSelections[]=
@@ -268,11 +268,11 @@ class CfgVehicles
 
 	class 24th_Druid_Rucksack_Basic_Desert_LR: 24th_Druid_Rucksack_Basic_Desert
 	{
-		dlc="24th Aux";
-		author="Ithias";
+		dlc="24th RB Aux";
+		author="Veta";
         scope=2;
         scopeCurator=2;
-		displayName="[24th] Athena Rucksack (NCO LR / Desert)";
+		displayName="[24th RB] Druid Rucksack (NCO LR / Desert)";
 		model="\OPTRE_unsc_units\army\rucksack.p3d";
 		maximumLoad=350;
 		mass=40;
@@ -286,7 +286,7 @@ class CfgVehicles
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"24th_AB_AUX\Equipment\Backpacks\Tex\24th_Athena_Rucksack_Desert_LR.paa",
+			"24th_RB_AUX\Equipment\Backpacks\Tex\24th_Druid_Rucksack_Desert_LR.paa",
 			""
 		};
 		tf_isolatedAmount=0.64999998;
@@ -302,11 +302,11 @@ class CfgVehicles
 
 	class 24th_Druid_Rucksack_Basic_Snow: OPTRE_UNSC_Rucksack
 	{
-		dlc="24th Aux";
-		author="Ithias";
+		dlc="24th RB Aux";
+		author="Veta";
         scope=2;
         scopeCurator=2;
-		displayName="[24th] Athena Rucksack (Basic / Snow)";
+		displayName="[24th RB] Druid Rucksack (Basic / Snow)";
 		model="\OPTRE_unsc_units\army\rucksack.p3d";
 		maximumLoad=350;
 		mass=40;
@@ -320,16 +320,16 @@ class CfgVehicles
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"24th_AB_AUX\Equipment\Backpacks\Tex\24th_Athena_Rucksack_Snow.paa",
+			"24th_RB_AUX\Equipment\Backpacks\Tex\24th_Druid_Rucksack_Snow.paa",
 			""
 		};
 	};
 
 	class 24th_Druid_Rucksack_Heavy_Snow: 24th_Druid_Rucksack_Basic_Snow
 	{
-		dlc="24th Aux";
-		author="Ithias";
-		displayName="[24th] Athena Rucksack (Heavy / Snow)";
+		dlc="24th RB Aux";
+		author="Veta";
+		displayName="[24th RB] Druid Rucksack (Heavy / Snow)";
 		maximumLoad=400;
 		mass=50;
 		hiddenSelections[]=
@@ -343,9 +343,9 @@ class CfgVehicles
 
 	class 24th_Druid_Rucksack_Medic_Snow: 24th_Druid_Rucksack_Basic_Snow
 	{
-		dlc="24th Aux";
-		author="Ithias";
-		displayName="[24th] Athena Rucksack (Medic / Snow)";
+		dlc="24th RB Aux";
+		author="Veta";
+		displayName="[24th RB] Druid Rucksack (Medic / Snow)";
         maximumLoad=400;
         mass=50;
 		hiddenSelections[]=
@@ -359,11 +359,11 @@ class CfgVehicles
 
 	class 24th_Druid_Rucksack_Basic_Snow_LR: 24th_Druid_Rucksack_Basic_Snow
 	{
-		dlc="24th Aux";
-		author="Ithias";
+		dlc="24th RB Aux";
+		author="Veta";
         scope=2;
         scopeCurator=2;
-		displayName="[24th] Athena Rucksack (NCO LR / Snow)";
+		displayName="[24th RB] Druid Rucksack (NCO LR / Snow)";
 		model="\OPTRE_unsc_units\army\rucksack.p3d";
 		maximumLoad=350;
 		mass=40;
@@ -377,7 +377,7 @@ class CfgVehicles
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"24th_AB_AUX\Equipment\Backpacks\Tex\24th_Athena_Rucksack_Snow_LR.paa",
+			"24th_RB_AUX\Equipment\Backpacks\Tex\24th_Druid_Rucksack_Snow_LR.paa",
 			""
 		};
 		tf_isolatedAmount=0.64999998;
@@ -389,130 +389,16 @@ class CfgVehicles
         tf_hasLRradio=1;
 	};
 
-
-//Satchels
-
-    class 24th_Druid_Satchel_Medic: OPTRE_ONI_Researcher_Suitcase
-    {
-		author="Ithias";
-		dlc="24th Aux";
-		scope=2;
-		scopeArsenal=2;
-		displayName="[24th] Athena Satchel (Medic)";
-		/*
-		tf_isolatedAmount=0.64999998;
-		tf_encryptionCode="tf_west_radio_code";
-		tf_dialog="anarc210_radio_dialog";
-		tf_subtype="digital_lr";
-		tf_range=120000;
-		tf_dialogUpdate="[""%1""] call TFAR_fnc_updateLRDialogToChannel;";
-		tf_hasLRradio=1; 
-		*/
-		hiddenSelections[]=
-		{
-			"camo"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"24th_AB_AUX\Equipment\Backpacks\Tex\24th_Athena_Satchel_Medic.paa",
-		}; 
-		maximumLoad=400;
-		mass=1;
-	};
-	
-    class 24th_Druid_Satchel_Large: OPTRE_ONI_Researcher_Suitcase
-    {
-		author="Ithias";
-		dlc="24th Aux";
-		scope=2;
-		scopeArsenal=2;
-		displayName="[24th] Athena Satchel (Basic / Large)";
-		/*
-		tf_isolatedAmount=0.64999998;
-		tf_encryptionCode="tf_west_radio_code";
-		tf_dialog="anarc210_radio_dialog";
-		tf_subtype="digital_lr";
-		tf_range=120000;
-		tf_dialogUpdate="[""%1""] call TFAR_fnc_updateLRDialogToChannel;";
-		tf_hasLRradio=1; 
-		*/
-		hiddenSelections[]=
-		{
-			"camo"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"24th_AB_AUX\Equipment\Backpacks\Tex\24th_Athena_Satchel_Large.paa",
-		}; 
-		maximumLoad=350;
-		mass=1;
-	};
-
-    class 24th_Druid_Satchel_Small: OPTRE_ONI_Researcher_Suitcase
-    {
-		author="Ithias";
-		dlc="24th Aux";
-		scope=2;
-		scopeArsenal=2;
-		displayName="[24th] Athena Satchel (Basic / Small)";
-		/*
-		tf_isolatedAmount=0.64999998;
-		tf_encryptionCode="tf_west_radio_code";
-		tf_dialog="anarc210_radio_dialog";
-		tf_subtype="digital_lr";
-		tf_range=120000;
-		tf_dialogUpdate="[""%1""] call TFAR_fnc_updateLRDialogToChannel;";
-		tf_hasLRradio=1; 
-		*/
-		hiddenSelections[]=
-		{
-			"camo"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"24th_AB_AUX\Equipment\Backpacks\Tex\24th_Athena_Satchel_Small.paa",
-		}; 
-		maximumLoad=350;
-		mass=1;
-	};
-
-//Invisable
-
-	class 24th_Druid_Rucksack_Basic_Invisable: OPTRE_UNSC_Rucksack
-	{
-		dlc="24th Aux";
-		author="Ithias";
-        scope=2;
-        scopeCurator=2;
-		displayName="[24th] Athena Rucksack (Invisable)";
-		model="";
-		maximumLoad=350;
-		mass=40;
-		hiddenSelections[]=
-		{
-			"camo",
-			"camo2",
-			"B_Addons",
-			"B_Medic",
-			"B_Radio"
-		};
-		hiddenSelectionsTextures[]=
-		{
-			"24th_AB_AUX\Equipment\Backpacks\Tex\24th_Athena_Rucksack_Woodland.paa",
-			""
-		}; 
-	};
-
 //NCO LR
 
 
 	class 24th_Druid_Rucksack_NCO_LR: OPTRE_ANPRC_515
 	{
-		dlc="24th Aux";
-		author="Ithias";
+		dlc="24th RB Aux";
+		author="Veta";
         scope=2;
         scopeCurator=2;
-		displayName="[24th] Athena Rucksack (NCO LR)";
+		displayName="[24th RB] Druid Rucksack (NCO LR)";
 		maximumLoad=350;
 		mass=40;
 	};
@@ -520,9 +406,9 @@ class CfgVehicles
 //Longbow
 	class 24th_Pilot_Parachute: B_Parachute
 	{
-		author="Ithias";
+		author="Veta";
 		scope=2;
-		displayName="[24th] Longbow Parachute";
+		displayName="[24th RB] Falcon Parachute";
 		maximumLoad=0;
 		mass=1;
 		tf_isolatedAmount=0.64999998;
@@ -542,15 +428,15 @@ class CfgVehicles
 
 	class 24th_Druid_Rucksack_JTAC_Urban: OPTRE_ANPRC_521_Green
 	{
-		dlc="24th Aux";
-		author="Ithias";
+		dlc="24th RB Aux";
+		author="Veta";
 		scope=2;
 		scopeCurator=2;
-		displayName="[24th] Athena Rucksack (JTAC / Urban)";
+		displayName="[24th RB] Druid Rucksack (JTAC / Urban)";
 		hiddenSelectionsTextures[]=
 		{
 			"optre_unsc_units\army\data\soft_backpack_co.paa",
-			"24th_AB_AUX\Equipment\Backpacks\Tex\24th_Athena_Rucksack_JTAC_Urban.paa",
+			"24th_RB_AUX\Equipment\Backpacks\Tex\24th_Druid_Rucksack_JTAC_Urban.paa",
 		}; 
 		descriptionShort="AN/PRC-521<br>UNSC Long Range Radio<br>30km Effective Range";
 		tf_isolatedAmount=0.64999998;
@@ -565,15 +451,15 @@ class CfgVehicles
 
 	class 24th_Druid_Rucksack_JTAC_Desert: OPTRE_ANPRC_521_Green
 	{
-		dlc="24th Aux";
-		author="Ithias";
+		dlc="24th RB Aux";
+		author="Veta";
 		scope=2;
 		scopeCurator=2;
-		displayName="[24th] Athena Rucksack (JTAC / Desert)";
+		displayName="[24th RB] Druid Rucksack (JTAC / Desert)";
 		hiddenSelectionsTextures[]=
 		{
 			"optre_unsc_units\army\data\soft_backpack_co.paa",
-			"24th_AB_AUX\Equipment\Backpacks\Tex\24th_Athena_Rucksack_JTAC_Desert.paa",
+			"24th_RB_AUX\Equipment\Backpacks\Tex\24th_Druid_Rucksack_JTAC_Desert.paa",
 		};
 		descriptionShort="AN/PRC-521<br>UNSC Long Range Radio<br>30km Effective Range";
 		tf_isolatedAmount=0.64999998;
@@ -588,15 +474,15 @@ class CfgVehicles
 
 	class 24th_Druid_Rucksack_JTAC_Woodland: OPTRE_ANPRC_521_Green
 	{
-		dlc="24th Aux";
-		author="Ithias";
+		dlc="24th RB Aux";
+		author="Veta";
 		scope=2;
 		scopeCurator=2;
-		displayName="[24th] Athena Rucksack (JTAC / Woodland)";
+		displayName="[24th RB] Druid Rucksack (JTAC / Woodland)";
 		hiddenSelectionsTextures[]=
 		{
 			"optre_unsc_units\army\data\soft_backpack_co.paa",
-			"24th_AB_AUX\Equipment\Backpacks\Tex\24th_Athena_Rucksack_JTAC_Woodland.paa",
+			"24th_RB_AUX\Equipment\Backpacks\Tex\24th_Druid_Rucksack_JTAC_Woodland.paa",
 		};
 		descriptionShort="AN/PRC-521<br>UNSC Long Range Radio<br>30km Effective Range";
 		tf_isolatedAmount=0.64999998;
@@ -611,15 +497,15 @@ class CfgVehicles
 	
 	class 24th_Druid_Rucksack_JTAC_Snow: OPTRE_ANPRC_521_Green
 	{
-		dlc="24th Aux";
-		author="Ithias";
+		dlc="24th RB Aux";
+		author="Veta";
 		scope=2;
 		scopeCurator=2;
-		displayName="[24th] Athena Rucksack (JTAC / Snow)";
+		displayName="[24th RB] Druid Rucksack (JTAC / Snow)";
 		hiddenSelectionsTextures[]=
 		{
 			"optre_unsc_units\army\data\soft_backpack_co.paa",
-			"24th_AB_AUX\Equipment\Backpacks\Tex\24th_Athena_Rucksack_JTAC_Snow.paa",
+			"24th_RB_AUX\Equipment\Backpacks\Tex\24th_Druid_Rucksack_JTAC_Snow.paa",
 		};
 		descriptionShort="AN/PRC-521<br>UNSC Long Range Radio<br>30km Effective Range";
 		tf_isolatedAmount=0.64999998;
